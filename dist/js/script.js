c713364373e119d1ec97c2e18c9bc22559d4a583
sammiesleek <samlocomotive1@gmail.com>
2022-05-26 13:43:05 +0100

    home page completed

--- dist/js/script.js
+++ dist/js/script.js
@@ -22,10 +22,181 @@ $("#homesec .owl-carousel").owlCarousel({
 
 })
 
+$(".top-deals .owl-carousel").owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    autoplayTimeout:5000,
+    // autoplayHoverPause:false,
+    // animateIn: 'animate_flipInX',
+    // animateOut:'animate_slideOutUp', 
+    // animateIn:'animate__bounceInDown',
+    autoplay:true,
+     lazyLoad:false,
+     mouseDrag:true,
+    //  margin:10,
+     responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+            
+           
+          
+        }
+
+})
+$(".new-arrival .owl-carousel").owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    autoplayTimeout:5000,
+    // autoplayHoverPause:false,
+    // animateIn: 'animate_flipInX',
+    // animateOut:'animate_slideOutUp', 
+    // animateIn:'animate__bounceInDown',
+    autoplay:true,
+     lazyLoad:false,
+     mouseDrag:true,
+    //  margin:10,
+     responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+            
+           
+          
+        }
+
+})
+$(".top-selling .owl-carousel").owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    autoplayTimeout:5000,
+    // autoplayHoverPause:false,
+    // animateIn: 'animate_flipInX',
+    // animateOut:'animate_slideOutUp', 
+    // animateIn:'animate__bounceInDown',
+    autoplay:true,
+     lazyLoad:false,
+     mouseDrag:true,
+    //  margin:10,
+     responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            },
+            1200:{
+                items:6
+            }
+            
+           
+          
+        }
+
+})
+$(".banner1 .owl-carousel").owlCarousel({
+    loop:false,
+    nav:false,
+    dots:true,
+    // autoplayTimeout:20000,
+    autoplayHoverPause:false,
+    // animateIn: 'animate_flipInX',
+    // animateOut:'animate_slideOutUp', 
+    // animateIn:'animate__bounceInDown',
+    autoplay:false,
+     lazyLoad:false,
+     mouseDrag:true,
+     margin:10,
+     responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+            
+           
+          
+        }
+
+})
+$(".banner2 .owl-carousel").owlCarousel({
+    loop:false,
+    nav:false,
+    dots:true,
+    // autoplayTimeout:20000,
+    autoplayHoverPause:false,
+    // animateIn: 'animate_flipInX',
+    // animateOut:'animate_slideOutUp', 
+    // animateIn:'animate__bounceInDown',
+    autoplay:false,
+     lazyLoad:false,
+     mouseDrag:true,
+     margin:10,
+     responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+           
+            
+           
+          
+        }
+
+})
+
 
 
 $(document).ready(()=>{
 
      $(".owl-nav .owl-next").html(`<i class='bx bx-chevron-right'></i>`)
     $(".owl-nav .owl-prev").html(`<i class='bx bx-chevron-left'></i>`)
+})
+
+
+$(document).ready(()=>{
+
+     //  isotope filter  recent properties
+    var $grid= $(".grid").isotope({
+        itemSelector:'.items',
+        layoutMode:'fitRows'
+    })
+    $(".button-group").on("click", "button",function(e){
+        $("#sfg .items").removeClass('off')
+        $(".button-group button").removeClass('active')
+        var filterValue=$(this).attr('data-filter');
+       $(this).addClass('active')
+       $grid.isotope({filter:filterValue})
+      
+    })
 })
